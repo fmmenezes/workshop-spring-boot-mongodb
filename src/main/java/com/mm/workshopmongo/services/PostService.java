b46ca190832638ac64a787e08e6d3c935e542636
fmmenezes <fmmenezes@hotmail.com>
2020-08-28 11:22:25 -0300

    Pesquisa posts por ID

---
+++ src/main/java/com/mm/workshopmongo/services/PostService.java
@@ -0,0 +1,26 @@
+package com.mm.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mm.workshopmongo.domain.Post;
+import com.mm.workshopmongo.repository.PostRepository;
+import com.mm.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+
+		Optional<Post> usuario = repo.findById(id);
+
+		return usuario.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+
+	}
+
+}
