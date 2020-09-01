309dab268b9aac7cc03e1aee7b2af468ee602bd6
fmmenezes <fmmenezes@hotmail.com>
2020-09-01 11:15:44 -0300

    Consulta finByTitle com query methods

--- src/main/java/com/mm/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/mm/workshopmongo/repository/PostRepository.java
@@ -1,9 +1,13 @@
 package com.mm.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.mm.workshopmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	List<Post> findByTitleContaining(String text);
 
 }
--- src/main/java/com/mm/workshopmongo/services/PostService.java
+++ src/main/java/com/mm/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.mm.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,12 @@ public class PostService {
 		return usuario.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 
 	}
+	
+	public List<Post> findByTitle(String text){
+		
+		return repo.findByTitleContaining(text);
+	}
+	
+	
 
 }
